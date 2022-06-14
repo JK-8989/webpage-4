28bf65c3fc2e719175f69b483f8a3a6703f2e023
JK-8989 <jiyoungkim365@gmail.com>
2022-06-13 22:26:24 -0700

    Add search.html / css files & search function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let searchParam = location.search.split('=').pop();
+
 const access_key = 'ESDjSj2JEgvd-Fhuolf4mGHaIagjWFVWZpTh8dLNp20';
 
 const random_photo_url = `https://api.unsplash.com/photos/random?client_id=${access_key}&count=30`;
+const search_photo_url = `https://api.unsplash.com/search/photos?client_id=${access_key}&query=${searchParam}&per_page=50`;
 
 const gallery = document.querySelector('.gallery');
 
@@ -16,6 +19,16 @@ const getImages = () => {
         makeImages(allImages);
     });
 }
+
+const searchImages = () => {
+  fetch(search_photo_url)
+  .then(res => res.json())
+  .then(data => {
+      allImages = data.results;
+      makeImages(allImages);
+  });
+}
+
 
 const makeImages = (data) => {
   data.forEach((item, index) => {
@@ -42,6 +55,32 @@ const showPopup = (item) => {
   downloadBtn.href = item.links.html;
   image.src = item.urls.regular;
 
+  closeBtn.addEventListener('click', ()=>{
+    popup.classList.add('hide');
+  })
+
+}
+if(searchParam == ''){
+  getImages();
+}else{
+  searchImages();
 }
 
-getImages()
+getImages();
+
+// pre & next button arrows
+const preBtn = document.querySelector('.pre-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+preBtn.addEventListener('click', () => {
+  if(current_image > 0){
+    current_image--;
+    showPopup(allImages[current_image]);
+  } 
+})
+nextBtn.addEventListener('click', () => {
+  if(current_image < allImages.length -1){
+    current_image++;
+    showPopup(allImages[current_image]);
+  } 
+})
