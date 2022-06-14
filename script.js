7b29991f56b2cdd760a1239f2eabe72c05db97f0
JK-8989 <jiyoungkim365@gmail.com>
2022-06-13 20:16:47 -0700

    Add popup function

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-const access_key = 'kZgLlrhYYwDB80VmUxDdydI7lGb_5PSazfbo31hrUKI';
+const access_key = 'ESDjSj2JEgvd-Fhuolf4mGHaIagjWFVWZpTh8dLNp20';
 
 const random_photo_url = `https://api.unsplash.com/photos/random?client_id=${access_key}&count=30`;
 
 const gallery = document.querySelector('.gallery');
 
+
 let allImages; // this will store all the images
+let current_image;
 
 const getImages = () => {
     fetch(random_photo_url)
     .then(res => res.json())
     .then(data => {
-      allImages = data;
-      makeImages(allImages);
-    })
-  
+        allImages = data;
+        makeImages(allImages);
+    });
 }
 
 const makeImages = (data) => {
@@ -24,6 +25,23 @@ const makeImages = (data) => {
     img.className = 'gallery-img';
 
     gallery.appendChild(img);
+
+    img.addEventListener('click', () => {
+      current_image = index;
+      showPopup(item);
+    })
   })
 }
+const showPopup = (item) => {
+  let popup = document.querySelector('.image-popup');
+  const downloadBtn = document.querySelector('.download-btn');
+  const closeBtn = document.querySelector('.close-btn');
+  const image = document.querySelector('.large-img');
+
+  popup.classList.remove('hide');
+  downloadBtn.href = item.links.html;
+  image.src = item.urls.regular;
+
+}
+
 getImages()
